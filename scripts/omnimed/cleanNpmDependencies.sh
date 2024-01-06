ab71d084edcb255d196be447471d12695a470eed
gcharland <gcharland@omnimed.com>
2024-01-06 01:16:31 -0500

    Pimp mon json - Retire les lignes qui correspondend au unused packages
    TODO:
    Checker si les dépendances "dev" sont downloadées en Test / Stage
    Si elles sont downloadées, passer un paramètre pour être sur que jenkins l'ignore

--- scripts/omnimed/cleanNpmDependencies.sh
+++ scripts/omnimed/cleanNpmDependencies.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/bash
 
-
 SOL_LIST=$(ls ~/git/Omnimed-solutions | grep omnimed-frontend-)
 
-IFS=$'\n' read -r -d '' -a FRONTEND_SOLUTION_LIST < <( ls ~/git/Omnimed-solutions/ | grep omnimed-frontend && printf '\0' )
+IFS=$'\n' read -r -d '' -a SOL_LIST < <( ls ~/git/Omnimed-solutions/ | grep omnimed-frontend && printf '\0' )
 
-#for DIR in "${FRONTEND_SOLUTION_LIST[@]}"; do
-#    echo cd to $DIR
-#    cd $HOME/git/Omnimed-solutions/$DIR
-#    depcheck > unused_dependencies.txt || true
-#done
-#
-for DIR in "${FRONTEND_SOLUTION_LIST[@]}"; do
+#for DIR in "omnimed-frontend-prescribeit"; do
+for DIR in "${SOL_LIST[@]}"; do
     cd $HOME/git/Omnimed-solutions/$DIR
-    cat unused_dependencies.txt
-done
+    depcheck > unused_dependencies.txt || true
 
+    echo "$DIR:"
+    echo "  - Total of $(wc -l unused_dependencies.txt)"
+    sed -i '0,/^Unused dependencies/d' unused_dependencies.txt
+    sed -i '/^Unused devDependencies$/,$d' unused_dependencies.txt
+    sed -i 's/.*\* //' unused_dependencies.txt 
+    IFS=$'\n' read -r -d '' -a DEP_LIST < <( cat unused_dependencies.txt )
+
+    echo "  - Will remove $(wc -l unused_dependencies.txt)"
+    echo "---------------------"
+    for DEP in "${DEP_LIST[@]}"; do
+        sed -i "/.*${DEP}.*/d" package.json
+    done
+done
+echo "Done. See git diff for details"
