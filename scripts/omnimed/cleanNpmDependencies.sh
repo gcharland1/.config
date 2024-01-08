716b9f92d279daf1161efbfb05f04d148fda7477
gcharland <gcharland@omnimed.com>
2024-01-08 11:02:49 -0500

    Clean le script. Pas utile encore :shrug:

--- scripts/omnimed/cleanNpmDependencies.sh
+++ scripts/omnimed/cleanNpmDependencies.sh
@@ -4,22 +4,25 @@ SOL_LIST=$(ls ~/git/Omnimed-solutions | grep omnimed-frontend-)
 
 IFS=$'\n' read -r -d '' -a SOL_LIST < <( ls ~/git/Omnimed-solutions/ | grep omnimed-frontend && printf '\0' )
 
-#for DIR in "omnimed-frontend-prescribeit"; do
+#for DIR in "omnimed-frontend-angularjs"; do
 for DIR in "${SOL_LIST[@]}"; do
     cd $HOME/git/Omnimed-solutions/$DIR
-    depcheck > unused_dependencies.txt || true
+    echo $DIR
+    unused=$(depcheck --oneline --ignores="omnimed-*, diacritics, \@angular*" || true)
+    unused=$(echo $unused | sed 's/Unused devDependencies.*//' )
+    unused=$(echo $unused | sed 's/^Unused dependencies//')
 
-    echo "$DIR:"
-    echo "  - Total of $(wc -l unused_dependencies.txt)"
-    sed -i '0,/^Unused dependencies/d' unused_dependencies.txt
-    sed -i '/^Unused devDependencies$/,$d' unused_dependencies.txt
-    sed -i 's/.*\* //' unused_dependencies.txt 
-    IFS=$'\n' read -r -d '' -a DEP_LIST < <( cat unused_dependencies.txt )
-
-    echo "  - Will remove $(wc -l unused_dependencies.txt)"
-    echo "---------------------"
+    IFS=' ' read -r -d '' -a DEP_LIST < <(echo $unused)
     for DEP in "${DEP_LIST[@]}"; do
-        sed -i "/.*${DEP}.*/d" package.json
+        rgxp=$(echo $DEP | sed 's/\//\\\//g')
+        rgxp=$(echo $rgxp | sed 's/\ //g')
+        rgxp=$(echo $rgxp | sed 's/-/\\\-/g')
+        rgxp="/${rgxp}/d"
+        echo $rgxp
+        sed -i $rgxp package.json
+        # TODO S'assurer de retirer les trailing "," si
+        #sed =i 's/\(.*\),\n\(.*}\)/\1\n\2/' package.json
     done
+    #npm run unicukes-dev-all
 done
 echo "Done. See git diff for details"
