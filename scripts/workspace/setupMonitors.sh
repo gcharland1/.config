24d07bd055fc0450e235934111a56bba69e583c7
gcharland <gcharland@omnimed.com>
2024-05-06 16:34:55 -0400

    i3wm config upgrade
    i3wm

--- scripts/workspace/setupMonitors.sh
+++ scripts/workspace/setupMonitors.sh
@@ -1,9 +1,9 @@
 # Setup tous les moniteurs par défaut (Reset)
-xrandr --output DP-1 --auto
-xrandr --output DP-2 --auto
-xrandr --output HDMI-1 --auto
-xrandr --output HDMI-1 --auto
+xrandr --output DP1 --auto
+xrandr --output DP2 --auto
+xrandr --output HDMI1 --auto
+xrandr --output HDMI2 --auto
 
-xrandr --output DP-1 --left-of eDP-1
-xrandr --output DP-2 --left-of eDP-1
-xrandr --output HDMI-1 --right-of eDP-1
+xrandr --output DP1 --left-of eDP1
+xrandr --output DP2 --left-of eDP1
+xrandr --output HDMI1 --right-of eDP1
