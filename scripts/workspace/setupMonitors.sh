5c97ad11d53dadc4121586042c993048b633d37a
gcharland <gcharland@omnimed.com>
2024-06-04 20:40:17 -0400

    start tmux and setup monitors

--- scripts/workspace/setupMonitors.sh
+++ scripts/workspace/setupMonitors.sh
@@ -4,6 +4,15 @@ xrandr --output DP2 --auto
 xrandr --output HDMI1 --auto
 xrandr --output HDMI2 --auto
 
-xrandr --output DP1 --left-of eDP1
-xrandr --output DP2 --left-of eDP1
+
+WIFI_NAME=$(iwgetid -r)
+
+if [ $WIFI_NAME = "BELL314" ]; then
+    xrandr --output DP1 --right-of eDP1
+    xrandr --output DP2 --right-of eDP1
+else
+    xrandr --output DP1 --left-of eDP1
+    xrandr --output DP2 --left-of eDP1
+fi
+
 xrandr --output HDMI1 --right-of eDP1
