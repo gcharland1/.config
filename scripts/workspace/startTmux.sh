1ea6817f0201a81fdcfce9d81384434ee10fe7a9
gcharland <gcharland@omnimed.com>
2024-06-05 18:16:15 -0400

    initialiser les terminaux en commencant la session

--- scripts/workspace/startTmux.sh
+++ scripts/workspace/startTmux.sh
@@ -1,8 +1,8 @@
 tmux -f ~/.config/tmux/tmux.conf new-session -d -s ssh -c ~
 tmux -f ~/.config/tmux/tmux.conf new-session -d -s omnimed -c ~/git/Omnimed-solutions
-tmux new-window -d -c "~/git/Omnimed-solutions/" -n "k9s" "k9s"
+tmux new-window -d -c "~/git/Omnimed-solutions/" -n "k9s" "k9s; zsh"
 tmux new-window -d -c "~/git/Omnimed-solutions/" -n "</>"
-tmux new-window -d -n "git" "cd ~/git/Omnimed-solutions/; lazygit"
+tmux new-window -d -n "git" "cd ~/git/Omnimed-solutions/; lazygit; zsh"
 
 # Use this to connect whenever you want 
 tmux attach -t omnimed
