da1681651609d26a841e4b5141e5cb69d9a90638
Gabriel Charland <gcharland@omnimed.com>
2026-07-16 11:13:03 -0400

    Pas de fuzzy-lock dans sway

--- scripts/workspace/fuzzy_lock.sh
+++ scripts/workspace/fuzzy_lock.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
 if [ -n "$WAYLAND_DISPLAY" ]; then
-    if (( $RANDOM % 3 )); then
-        swaylock --color 141414
-    else
-        # Take screenshot of the entire screen and blur it out
-        grim /tmp/fuzzylock.png
-        convert /tmp/fuzzylock.png -resize 16% /tmp/fuzzylock.png
-        convert /tmp/fuzzylock.png -resize 625% /tmp/fuzzylock.png
-
-        # Use swaylock to lock the screen with the screenshot as a background
-        swaylock -i /tmp/fuzzylock.png
-    fi
+    swaylock --color 141414
 else
     if (( $RANDOM % 3 )); then
         i3lock --nofork --color 141414
